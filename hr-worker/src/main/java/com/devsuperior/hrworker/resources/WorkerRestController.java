f7d1984603e8748ef2d0b00b31492d806813383f
EllEstevam <restevam@uolinc.com>
2022-07-02 18:33:42 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerRestController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerRestController.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,10 @@ import java.util.Optional;
 @RequestMapping(value = "/workers")
 public class WorkerRestController {
 
+   private static Logger logger = LoggerFactory.getLogger(WorkerRestController.class);
+
+   @Autowired
+   private Environment env;
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -28,6 +35,9 @@ public class WorkerRestController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> retrieveById(@PathVariable Long id) {
+
+        logger.info("Port"+env.getProperty("local.server.port"));
+
         Optional<Worker> workerOptional = workerRepository.findById(id);
 
         if (workerOptional.isPresent()) {
